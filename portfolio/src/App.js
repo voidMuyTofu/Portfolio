365ed89ab57440ac296ce6326b4bf0100b0f7070
Fernando <fernandoperezdelatorremunoz@gmail.com>
2021-08-20 14:55:59 +0200

    Añadido panel de navegacion lateral

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,20 @@
-function App() {
-  return (<div>
-    </div>
-  );
+import React from 'react';
+import '@fontsource/roboto';
+import svg from './Recursos/cool-background.svg';
+import './App.css'
+import MiniDrawer from './Componentes/Menu';
+import { Grid } from '@material-ui/core'
+import { width } from '@material-ui/system';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={'fondo'}>
+        <MiniDrawer/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
